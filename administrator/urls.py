6bead2e14e45bce2fd540d31971a047dcc3bd3a6
SafeKerala <abhinavv21245@gmail.com>
2024-12-28 17:07:38 +0530

    second commit

--- administrator/urls.py
+++ administrator/urls.py
@@ -5,20 +5,23 @@ from .views import *
 urlpatterns = [
     path('admindashboard',admindashboard.as_view(),name='admindashboard'),
     path('viewcomplaints',viewcomplaints.as_view(),name='viewcomplaints'),
-    path('viewcriminal',viewcriminals.as_view(),name='viewcriminals'),
     path('viewlabours',viewlabours.as_view(),name='viewlabours'),
     path('viewpolicestation',viewpolicestation.as_view(),name='viewpolicestation'),
-    path('login',login.as_view(),name='login'),
-    path('editcriminals',editcriminals.as_view(),name='editcriminals'),
-    path('editlabours',editlabours.as_view(),name='editlabours'),
-    path('policestationdashboard',policestationdashboard.as_view(),name='policestationdashboard'),
-    path('register',register.as_view(),name='register'),
-    path('replycomplaint',replycomplaint.as_view(),name='replycomplaint'),
     path('sendnotification',sendnotification.as_view(),name='sendnotification'),
-    path('sendsolution',sendsolution.as_view(),name='sendsolution'),
     path('viewfeedback',viewfeedback.as_view(),name='viewfeedback'),
-    
-    
+    path('replycomplaint',replycomplaint.as_view(),name='replycomplaint'),
+    path('policestationdashboard',policestationdashboard.as_view(),name='policestationdashboard'),
+    path('criminallistmanagement',criminallistmanagement.as_view(),name='criminallistmanagement'),
+    path('sendsolution',sendsolution.as_view(),name='sendsolution'),
+     path('userdashboard',userdashboard.as_view(),name='userdashboard'),
+    path('addcomplaint',addcomplaint.as_view(),name='addcomplaint'),
+    path('sendfeedback',sendfeedback.as_view(),name='sendfeedback'),
+    path('sendrequestforlabours',sendrequestforlabours.as_view(),name='sendrequestforlabours'),
+    path('viewrequeststatus',viewrequeststatus.as_view(),name='viewrequeststatus'),
 
+
+    
+path('editcriminals',editcriminals.as_view(),name='editcriminals'),
+    path('editlabours',editlabours.as_view(),name='editlabours'),
 ]
 
--- administrator/views.py
+++ administrator/views.py
@@ -3,47 +3,65 @@ from django.views import View
 
 # Create your views here.
 class admindashboard(View):
-    def get(self,request)
+    def get(self,request):
         return render(request,'admin/admindashboard.html')
 class viewcomplaints(View):
-    def get(self,request)
+    def get(self,request):
         return render(request,'admin/viewcomplaints.html')
-class viewcriminal(View):
-    def get(self,request)
-        return render(request,'admin/viewcriminals.html')  
 class viewlabours(View):
-    def get(self,request)
+    def get(self,request):
         return render(request,'admin/viewlabours.html')
 class viewpolicestation(View):
-    def get(self,request)
+    def get(self,request):
         return render(request,'admin/viewpolicestation.html') 
-class login(View):
-    def get(self,request)
-        return render(request,'admin/login.html')
+class sendnotification(View):
+    def get(self,request):
+        return render(request,'admin/sendnotification.html') 
+class viewfeedback(View):
+    def get(self,request):
+        return render(request,'admin/viewfeedback.html')
+class replycomplaint(View):
+    def get(self,request):
+        return render(request,'admin/replycomplaint.html')
+class policestationdashboard(View):
+    def get(self,request):
+        return render(request,'police/policestationdashboard.html')            
+class criminallistmanagement(View):
+    def get(self,request):
+        return render(request,'police/criminallistmanagement.html') 
+class sendsolution(View):
+    def get(self,request):
+        return render(request,'police/sendsolution.html')
+class userdashboard(View):
+    def get(self,request):
+        return render(request,'user/userdashboard.html')        
+class addcomplaint(View):
+    def get(self,request):
+        return render(request,'user/addcomplaint.html')        
+class sendfeedback(View):
+    def get(self,request):
+        return render(request,'user/sendfeedback.html')        
+class sendrequestforlabours(View):
+    def get(self,request):
+        return render(request,'user/sendrequestforlabours.html')                
+class viewrequeststatus(View):
+    def get(self,request):
+        return render(request,'user/viewrequeststatus.html')        
+
+
+
+
+
+
+
+
 class editcriminals(View):
-    def get(self,request)
+    def get(self,request):
         return render(request,'editcriminals.html')       
 class editlabours(View):
-    def get(self,request)
-        return render(request,'editlabours.html') 
-class policestationdashboard(View):
-    def get(self,request)
-        return render(request,'policestationdashboard.html') 
-class register(View):
-    def get(self,request)
-        return render(request,'register.html')                                     
-class replycomplaint(View):
-    def get(self,request)
-        return render(request,'replycomplaint.html') 
-class sendnotification(View):
-    def get(self,request)
-        return render(request,'sendnotification.html')
-class sendsolution(View):
-    def get(self,request)
-        return render(request,'sendsolution.html')
- class viewfeedback(View):
-    def get(self,request)
-        return render(request,'viewfeedback.html')        
+    def get(self,request):
+        return render(request,'editlabours.html')                                   
+
                
 
                 
